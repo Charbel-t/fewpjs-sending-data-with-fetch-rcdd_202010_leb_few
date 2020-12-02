7c3e9d6273b57ace404298a40efd307d88675750
Charbel t <charbel2000.52@gmail.com>
2020-12-02 10:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
-// Add your code here
-const testvar={};
-function testFunc(){
-  return "hi";
-}
-
-function registerSelf(){
-  return fetch("http://guestbook.example.com/register",{
-    method: "POST",
-    body:{
-      firstName: "Charbel",
-      registryMessage: "Registered"
-    }
-  })
-  .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      return json.message;
-    });
-}
-
-let formData = {
-  firstName: "Charbel",
-  registryMessage: "This is a message"
-};
-
-let configObj = {
-  method: "POST",
-  body: formData
-};
-
-function errorSelf() {
-  return fetch("http://guestbook.example.com/register-error", configObj)
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(json) {
-      return json.messsage;
-    })
-    .catch(function(x) {
-      return ":(";
-    });
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
 }
